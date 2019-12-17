b787667c56df6cc063742a68c6b1d365c5e82de4
akocik <agn.kocik@o2.pl>
2019-12-17 16:55:02 +0100

    Homework_fullVersion

--- src/test/java/org/vistula/restassured/information/HomeWorkRestAssured.java
+++ src/test/java/org/vistula/restassured/information/HomeWorkRestAssured.java
@@ -72,6 +72,41 @@ public class HomeWorkRestAssured extends RestAssuredTest {
                 .log().all()
                 .statusCode(200)
                 .body("name", equalTo("Kocik"));
+        given().delete("/information/"+userId)
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    public void shouldCorrectlyUpdateSalary () {
+        JSONObject requestParams = new JSONObject();
+        String myName = RandomStringUtils.randomAlphabetic(6);
+        requestParams.put("name", myName);
+        requestParams.put("nationality", "Poland");
+        requestParams.put("salary", 5000);
+        Information player = given()
+                .header("Content-Type", "application/json")
+                .body(requestParams.toString())
+                .post("/information" )
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract().body().as(Information.class);
+        long userId = player.getId();
+        JSONObject newPlayer = new JSONObject();
+        newPlayer.put("salary", 7000);
+        given().header("Content-Type", "application/json")
+                .body(newPlayer.toString())
+                .patch("/information/"+userId)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("salary", equalTo(7000));
+        given().delete("/information/"+userId)
+                .then()
+                .log().all()
+                .statusCode(204);
     }
 
 }
